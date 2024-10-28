706872b1ed77153f6a7a53ade43e88300a76ddb3
Asif  Ibrahim <AIbrahim20@DEV.LOCAL>
2024-10-28 20:01:38 +0000

    Notes App finished for now

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,13 @@
 // PUT - replaces the entair identifyed resource with the request data
 // PATCH - replaces a part of the identified resource with the request data
 
-
 // creating the first web server
 const express = require('express');
 const App = express();
 const port = 3001;
+
+// using express middleware
+App.use(express.json()); //express json parser
 
 let notes = [
     {
@@ -34,12 +36,31 @@ App.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
-// showing a specipfic resources
+// getting a specipfic resources
 App.get('/api/notes/:id', (req, res) => {
     const id = Number(req.params.id);
     const note = notes.find(note => note.id === id) //for finding a specific item
 
     note ? res.json(note) : res.status(404).json({ error: 'Note not found' });
+});
+
+//Adding Data through POST
+App.post('/api/notes', (req, res) => {
+    const { content, important = false } = req.body;
+
+    if (!content) {
+        return res.status(400).json({ error: "Please Add a content" })
+    };
+
+    // creating a new content
+    const newNote = {
+        id: notes.length ? Math.max(...notes.map(note => note.id)) + 1 : 1,
+        content,
+        important
+    };
+
+    notes.concat(newNote); //adding note to the array
+    res.status(201).json(newNote); //return the new note with status 201
 });
 
 // deleting a specigic resource
